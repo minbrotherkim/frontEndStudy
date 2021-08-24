b7158c44d5f517846db18a94090f9f43a470b413
minhyung_kim <minbrotherkim@gmail.com>
2021-08-24 17:07:27 +0900

    이벤트 위임을 통한 js 성능 개선

--- shoppingList/main.js
+++ shoppingList/main.js
@@ -1,64 +1,61 @@
-const input = document.querySelector('.footer_input')
-const addBtn = document.querySelector('.footer_button')
-const items = document.querySelector('.items')
+const input = document.querySelector(".footer_input");
+const addBtn = document.querySelector(".footer_button");
+const items = document.querySelector(".items");
 
 function addItem() {
-    const text = input.value
-    if(text === '') {
-        input.focus()
-        return;
-    }
+  const text = input.value;
+  if (text === "") {
+    input.focus();
+    return;
+  }
 
-    // 아이템 만들기 (itemRow)
-    const itemRow = createItem(text)
+  // 아이템 만들기 (itemRow)
+  const itemRow = createItem(text);
 
-    // 아이템 붙이기
-    items.appendChild(itemRow)
+  // 아이템 붙이기
+  items.appendChild(itemRow);
 
-    // 아이템으로 스크롤 옮기기
-    itemRow.scrollIntoView({block: 'center'})
+  // 아이템으로 스크롤 옮기기
+  itemRow.scrollIntoView({ block: "center" });
 
-    // 초기화
-    input.value=''
-    input.focus()
+  // 초기화
+  input.value = "";
+  input.focus();
 }
 
+let id = 0; // UUID
 function createItem(text) {
-    const itemRow = document.createElement('li')
-    itemRow.setAttribute('class', 'item_row')
-    
-    const item = document.createElement('div')
-    item.setAttribute('class', 'item')
+  const itemRow = document.createElement("li");
+  itemRow.setAttribute("class", "item_row");
+  itemRow.setAttribute('data-id', id)
+  itemRow.innerHTML = `
+    <div class="item">
+        <span class="item_name">${text}</span>
+        <button class="item_delete">
+            <i class="fas fa-trash-alt" data-id=${id}></i>
+        </button>
+    </div>
+    <div class="item_divider"></div>
+    `;
 
-    const name = document.createElement('span')
-    name.setAttribute('class', 'item_name')
-    name.innerText = text
-
-    const deleteBtn = document.createElement('button')
-    deleteBtn.setAttribute('class', 'item_delete')
-    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>'
-    deleteBtn.addEventListener('click', () => {
-        items.removeChild(itemRow)
-    })
-
-    const divider = document.createElement('div')
-    divider.setAttribute('class', 'item_divider')
-
-    item.appendChild(name)
-    item.appendChild(deleteBtn)
-
-    itemRow.appendChild(item)
-    itemRow.appendChild(divider)
-
-    return itemRow
+  id++
+  return itemRow;
 }
 
-addBtn.addEventListener('click', () => {
-    addItem()
-})
+addBtn.addEventListener("click", () => {
+  addItem();
+});
 
-input.addEventListener('keypress', (event) => {
-    if(event.key === 'Enter') {
-        addItem()
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    addItem();
+  }
+});
+
+items.addEventListener('click', event => {
+    const id = event.target.dataset.id
+    if(id){
+        const toBeDeleted = document.querySelector(`.item_row[data-id="${id}"]`)
+        toBeDeleted.remove()
     }
 })
