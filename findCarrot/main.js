e86ba507fc5d2d521591269a26e09e0fd8114ad1
minhyung_kim <minbrotherkim@gmail.com>
2021-08-27 17:41:21 +0900

    타이머 시작까지

---
+++ findCarrot/main.js
@@ -0,0 +1,97 @@
+'use strict'
+
+const CARROT_SIZE = 80
+const CARROT_COUNT = 5
+const BUG_COUNT = 5
+const GAME_DURATION_SEC = 5
+
+const field = document.querySelector('.game_field')
+const fieldRect = field.getBoundingClientRect()
+const gameBtn = document.querySelector('.game_btn')
+const gameTimer = document.querySelector('.game_timer')
+const gameScore = document.querySelector('.game_score')
+
+let started = false
+let score = 0
+let timer = undefined
+
+gameBtn.addEventListener('click', () => {
+    if(started) {
+        stopGame()
+    }
+    else {
+        startGame()
+    }
+
+    started = !started
+})
+
+function startGame() {
+    initGame()
+    showStopButton()
+    showTimerAndScore()
+    startGameTimer()
+}
+
+function stopGame() {
+
+}
+
+function showStopButton() {
+    const icon = gameBtn.querySelector('.fa-play')
+    icon.classList.add('fa-stop')
+    icon.classList.remove('fa-play')
+}
+
+function showTimerAndScore() {
+    gameScore.style.visibility = 'visible'
+    gameTimer.style.visibility = 'visible'
+}
+
+function startGameTimer() {
+    let remainingTimeSec = GAME_DURATION_SEC
+    updateTimerText(remainingTimeSec)
+    timer = setInterval(() => {
+        if(remainingTimeSec <= 0) {
+            clearInterval(timer)
+            return
+        }
+        updateTimerText(--remainingTimeSec)
+    }, 1000)
+}
+
+function updateTimerText(time) {
+    const minutes = Math.floor(time / 60)
+    const seconds = time % 60
+    gameTimer.innerText = `${minutes}:${seconds}`
+}
+
+function initGame() {
+    field.innerHTML=''
+    gameScore.innerText = CARROT_COUNT
+    // 벌레와 당근 생성 후 field에 추가
+    addItem('carrot', CARROT_COUNT, 'img/carrot.png')
+    addItem('bug', BUG_COUNT, 'img/bug.png')
+}
+
+function addItem(className, count, imgPath) {
+    const x1 = 0
+    const y1 = 0
+    const x2 = fieldRect.width - 80
+    const y2 = fieldRect.height - 80
+    for (let i = 0; i<count; i++) {
+        const item = document.createElement('img')
+        item.setAttribute('class', className)
+        item.setAttribute('src', imgPath)
+        item.style.position = 'absolute'
+        const x = randomNumber(x1, x2)
+        const y = randomNumber(y1, y2)
+        item.style.left = `${x}px`
+        item.style.top = `${y}px`
+        field.appendChild(item)
+    }
+}
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min
+}
